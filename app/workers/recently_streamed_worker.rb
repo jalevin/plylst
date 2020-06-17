08b1b05a4ce2168122778354906a29595688bb5a
Jeff Levin <jeff@levinology.com>
2020-06-16 18:38:32 -0800

    hackin- untested

--- app/workers/recently_streamed_worker.rb
+++ app/workers/recently_streamed_worker.rb
@@ -3,31 +3,25 @@ class RecentlyStreamedWorker
 
   sidekiq_options lock: :while_executing, on_conflict: :reject
 
+  # FIXME skylight doesn't like number of allocations. Let's microoptimize! (not tested)
   def perform(user_id)
-    user = User.find user_id
+    user = User.find_by(id: user_id, active: true)
+    return unless user
+    
+    spotify = RSpotify::User.new(user.settings.to_hash)
 
-    if user.active?
-      connection = user.settings.to_hash
-      spotify = RSpotify::User.new(connection)
-
-      begin
-        recent_tracks = spotify.recently_played(limit: 50)
-      rescue RestClient::Unauthorized, RestClient::BadRequest => e
-        user.increment!(:authorization_fails)
-
-        # Deactivate user if we don't have the right permissions and if their authorization has failed a crap ton of times
-        user.update_attribute(:active, false) if user.authorization_fails >= 10
+    begin
+      recent_track_ids = Array.new
+      spotify.recently_played(limit: 50).each do |track|
+        recent_track_ids.push([track.id, track.played_at])
       end
+    rescue RestClient::Unauthorized, RestClient::BadRequest => e
+      user.increment!(:authorization_fails)
 
-      if recent_tracks.present?
-        recent_track_ids = Array.new
-
-        recent_tracks.each do |track|
-          recent_track_ids.push([track.id, track.played_at])
-        end
-
-        SaveTracksWorker.perform_async(user.id, recent_track_ids, 'streamed')
-      end
+      # Deactivate user if we don't have the right permissions and if their authorization has failed a crap ton of times
+      user.update_attribute(:active, false) if user.authorization_fails >= 10
     end
+
+    SaveTracksWorker.perform_async(user.id, recent_track_ids, 'streamed') if recent_track_ids.present?
   end
 end
